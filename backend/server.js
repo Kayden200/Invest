b118268f50de16ae737fe42d9b3537e5670bd0b9
Kayden200 <159207295+Kayden200@users.noreply.github.com>
2025-04-17 21:52:34 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
-const transactionRoutes = require('./routes/transaction');
+const transactionRoutes = require('./routes/Transaction');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
